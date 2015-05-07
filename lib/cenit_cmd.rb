41fe792e5491026c6972db7f6abfb2511faa2ace
Miguel Sancho <sanchocuba@gmail.com>
2015-05-06 23:29:57 -0400

    fixed sintax issue

--- lib/cenit_cmd.rb
+++ lib/cenit_cmd.rb
@@ -2,11 +2,11 @@ require 'thor'
 require 'thor/group'
 
 case ARGV.first
-  when 'version', '-v', '--version'
-    puts Gem.loaded_specs['cenit_cmd'].version
-  when 'collection'
-    ARGV.shift
-    require 'cenit_cmd/collection'
-    CenitCmd::Collection.start
-  end
+when 'version', '-v', '--version'
+  puts Gem.loaded_specs['cenit_cmd'].version
+when 'collection'
+  ARGV.shift
+  require 'cenit_cmd/collection'
+  CenitCmd::Collection.start
 end
+
